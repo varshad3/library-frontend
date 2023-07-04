8474c294ba50652cd34a78100a83298905d5a6a6
varshad3 <varshasuresh169@gmail.com>
2023-07-04 12:55:19 +0530

    library-frontend

--- src/app/admin-issuereport/admin-issuereport.component.ts
+++ src/app/admin-issuereport/admin-issuereport.component.ts
@@ -21,4 +21,6 @@ constructor(private api:ApiService){
     })
   }
 
+  
+
 }
--- src/app/issued-books/issued-books.component.ts
+++ src/app/issued-books/issued-books.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-issued-books',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./issued-books.component.css']
 })
 export class IssuedBooksComponent {
+  itemBook:any
+  bookstatusmsg=''
+  constructor(private api:ApiService){
+
+  }
+  ngOnInit(): void {
+    this.api.requestgetbook().subscribe((result:any)=>{
+      console.log(result);
+      this.itemBook=result  
+    })
+  }
+clear(booktitle:any){
+  this.api.deletebook(booktitle).subscribe((result:any)=>{
+    this.itemBook=result;
+    window.location.reload()
+    if(this.itemBook.length===0){
+      this.bookstatusmsg = result.error.message;
+    }
+  },(result: any) => {
+    alert(result.error.message);
+  }
+  )
+}
 
 }
